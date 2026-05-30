17ef633efc07b19e9e3ad41717fa4015c345ec55
omegaserviceszorgui-cmd <omegaserviceszorgui@gmail.com>
2026-05-30 21:04:57 +0100

    Update KnowledgeCenter.tsx

--- omega-next/components/sections/KnowledgeCenter.tsx
+++ omega-next/components/sections/KnowledgeCenter.tsx
@@ -13,7 +13,7 @@ const guides = [
   { fr: "Guide Sécurité Sociale", ar: "دليل الضمان الاجتماعي", file: "securite-sociale/pension-vieillesse-tunisie.pdf", preview: "previews/pension-vieillesse.jpg" },
   { fr: "Guide de l' Étudiant ", ar: "دليل الطالب ", file: "etudiants/guide-pfe.pdf", preview: "previews/pfe-etudiants.png" },
   { fr: "Guide des Ressources Humaines ", ar: "دليل المواد البشرية ", file: "ressources-humaines/RH-gouvernance.pdf", preview: "previews/RH-preview.png" }
-];{ fr: "Guide de l'Administration ", ar: "دليل الخدمات الإدارية ", file: "administration/creation-societes.pdf", preview: "previews/RH-preview.png" },
+];{ fr: "Guide de l'Administration ", ar: "دليل الخدمات الإدارية ", file: "administration/creation-societes.pdf", preview: "previews/preview-societes.png" },
 ];
 
 const infographics = [
